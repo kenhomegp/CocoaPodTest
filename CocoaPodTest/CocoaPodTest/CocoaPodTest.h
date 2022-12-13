50115380f8c0b6d83431826301de00960ede5131
minglung <minglungh123@gmail.com>
2022-12-13 16:48:08 +0800

    Commit Implementation project files

---
+++ CocoaPodTest/CocoaPodTest/CocoaPodTest.h
@@ -0,0 +1,18 @@
+//
+//  CocoaPodTest.h
+//  CocoaPodTest
+//
+//  Created by Minglung on 2022/12/13.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CocoaPodTest.
+FOUNDATION_EXPORT double CocoaPodTestVersionNumber;
+
+//! Project version string for CocoaPodTest.
+FOUNDATION_EXPORT const unsigned char CocoaPodTestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CocoaPodTest/PublicHeader.h>
+
+
---
+++ CocoaPodTest/CocoaPodTest/PodTest.swift
@@ -0,0 +1,18 @@
+//
+//  PodTest.swift
+//  CocoaPodTest
+//
+//  Created by Minglung on 2022/12/13.
+//
+
+import Foundation
+
+public class PodTest {
+    let hello = "Hello"
+
+    public init() {}
+
+    public func hello(to whom: String) -> String {
+        return "Hello \(whom)"
+    }
+}
